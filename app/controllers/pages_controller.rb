ad22fbb27c7c553089bc96db01f3141e64f5aae0
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-17 16:45:47 +0200

    homepage with search bar, redirects to index workers... still protoype

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @jobrequest = JobRequest.new
   end
 end
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,9 +1,10 @@
 class WorkersController < ApplicationController
 
   def index
-    @workers = User.where(on_duty: true)\
-      .where('location LIKE %?%', params[:location])\
-      .where('position LIKE %?%', params[:position])
+    @workers = User.all
+    # @workers = User.where(on_duty: true)\
+    #   .where('location LIKE %?%', params[:location])\
+    #   .where('position LIKE %?%', params[:position])
   # Compléter avec la date demandée par l'employeur.
   end
 end
