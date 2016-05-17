ec19d3a011f1a89ceb100819db8ad1696058f85f
thomas rondio <sativva@free.fr>
2016-05-17 13:03:19 +0200

    first form is ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # get 'registrations/sign_up_params'
+
+  # get 'registrations/account_update_params'
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
