62956edce32847155de8bfc1b8128270c43b41bd
thomas rondio <sativva@free.fr>
2016-05-17 19:00:26 +0200

    fix git ignore problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true #:birth_date, :birth_place, :address, :position, presence: true
   validates :min_wage, numericality: { only_integer: true,  greater_than_or_equal_to: 750, allow_nil: true, message: 'must be greater than 7.5€ net/h' } # SMIC horaire 2016
-  # validates_date :birth_date, before: lambda { 16.year.ago }
+  validates_date :birth_date, before: lambda { 16.year.ago }
   validates :description,  length: { minimum: 100, maximum: 1000, allow_nil: true }
   validates :mobility_radius, numericality: { only_integer: true,  greater_than_or_equal_to: 0, allow_nil: true } # SMIC horaire 2016
   #2do add validation of secu_id
-  # validates :social_security_number, length: { is: 15 }
+  validates :social_security_number, length: { is: 15 }
   # t.string :first_name
   # t.string :last_name
   # t.date :birth_date
